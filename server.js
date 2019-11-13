42e9ea913af3f0decbc94fa3b8acb7b168d8cf0f
jdgriswo <jdgriswo@buffalo.edu>
2019-11-13 13:35:44 -0700

    The one and only submission oops haha so much for version control

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200,
+};
+
+var Request = require("request");
+
+app.use(bodyParser.json());
+app.use(cors(corsOptions));
+
+app.listen(8000, () => { console.log('server running'); });
+
+app.route('/api/anime').get((req, res) => {
+    /* const response = [{ name: 'Ghost in the Shell' }, { name: 'Spirited Away' }];
+    console.log(response);
+    res.send({
+        response
+    }); */
+    Request.get("https://api.jikan.moe/v3/top/anime/1/airing", (error, response, body) => {
+        if(error) {
+            return console.dir(error);
+        }
+        console.log('Got response');
+        res.status(200).send(JSON.parse(body).top);
+    });
+});
